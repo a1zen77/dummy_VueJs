34ca273e88c4c9e8d5951af76df855873fc62100
a1zen77 <3.5.4.soham@gmail.com>
2025-07-05 12:58:50 +0530

    base models added

---
+++ application/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String, unique=True, nullable=False)
+    fs_uniquifier = db.Column(db.String, unique = True, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
+    roles = db.relationship('Role', backref = 'bearer', secondary='user_roles')
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String(255))
+
+class UserRoles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
