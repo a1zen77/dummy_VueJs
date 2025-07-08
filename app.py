376ecfdc5b1fd5648e4d7d3211af257c9cd69711
a1zen77 <3.5.4.soham@gmail.com>
2025-07-08 16:58:22 +0530

    removed config file

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from application.database import db
 from application.config import LocalDevelopmentConfig
 from application.models import User, Role, UserRoles
 from flask_security import Security, SQLAlchemyUserDatastore
+from flask_security import hash_password
 
 def create_app():
     app = Flask(__name__)
@@ -17,6 +18,16 @@ def create_app():
     return app
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+
+    app.security.datastore.find_or_create_role(name='admin', description='Super User')
+    app.security.datastore.find_or_create_role(name='user', description='Regular User')
+    db.session.commit()
+
+    if not app.security.datastore.find_user(email = 'user@admin.com'):
+        app.security.datastore.create_user(email = 'user@admin.com', username = 'admin01', password = hash_password('12345'))
 
 if __name__ == "__main__":
     app.run()
