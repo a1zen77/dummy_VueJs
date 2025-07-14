1d1085247cf60549d0f340dd5bac50cc6a728588
a1zen77 <3.5.4.soham@gmail.com>
2025-07-14 13:08:58 +0530

    starting auth and roles

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,10 @@
 from flask import current_app as app
+from flask_security import auth_required, roles_required
 
 @app.route('/admin')
+@auth_required('token')
+@roles_required('admin')
 def admin_home():
-    return "<h1>This is Admin Page</h1>"
+    return {
+        "message" : "admin logged in successfully"
+    }
