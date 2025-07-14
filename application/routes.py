8c8d2ab66e030212c54d599f179c2b91e8c7f8e4
a1zen77 <3.5.4.soham@gmail.com>
2025-07-14 13:12:33 +0530

    jsonify

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,19 @@
-from flask import current_app as app
-from flask_security import auth_required, roles_required
+from flask import current_app as app, jsonify
+from flask_security import auth_required, roles_required, current_user
 
 @app.route('/admin')
 @auth_required('token')
 @roles_required('admin')
 def admin_home():
-    return {
+    return jsonify({
         "message" : "admin logged in successfully"
-    }
+    })
+
+@app.route('/user')
+def user_home(user_id):
+    user = current_user()
+    return jsonify({    
+        "username": user.username,
+        "email": user.email,
+        "password": user.password,
+    })
